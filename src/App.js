9c9f9501f4f14fea741b2226aa5d2029a8161340
Josué Lobo <darksiders20005@gmail.com>
2022-03-01 12:25:30 -0300

    feat: implementada base carrosel de projetos

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import Navbar from './components/navbar';
 import Section from './components/section';
 import Projects from './components/projects';
+import CarouselFront from './components/carouselFront'
+import CarouselBack from './components/carouselBack';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Section />
+      <CarouselFront />
+      <CarouselBack />
       <Projects />
     </div>
   );
